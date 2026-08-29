4dc3c371356751e393611dae2f3a92b357f717ad
ahmad <mk4249796@gmail.com>
2026-08-29 16:24:47 +0300

    implemented mock navbar

---
+++ djangoairbbn/app/components/navbar/SearchFilters.tsx
@@ -0,0 +1,13 @@
+const SearchFilters=()=>{
+    return(
+        <div className="h-16 flex flex-row items-center justify-between border border-gray-300 rounded-full">
+            <div className="flex flex-row items-center justify-between">
+                <div className="w-62.5 h-16 px-8 flex flex-col justify-center rounded-full hover:bg-gray-100 ">
+                    Search
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default SearchFilters
--- djangoairbbn/app/components/navbar/navbar.tsx
+++ djangoairbbn/app/components/navbar/navbar.tsx
@@ -1,7 +1,26 @@
+import Link from 'next/link';
+import Image from "next/image"
+import SearchFilters from './SearchFilters';
 const Navbar=()=>{
     return(
-        <nav>
-            Test
+        <nav className="w-full fixed top-0 left-0 py-6 border-b bg-white z-10 ">
+            <div className="max-w-[1500ps] mx-auto px-6">
+                <div className="flex justify-between items-center">
+                    <Link href="/">
+                    <Image alt="logo" src="/logo.png"
+                    width={180}
+                    height={38}
+                    />
+                    </Link>
+
+                    <div className='flex space-x-6'>
+                        <SearchFilters/>
+                    </div>
+                    <div className='flex items-center space-x-6'>
+                        Add property
+                    </div>
+                </div>
+            </div>
         </nav>
     )
 }
