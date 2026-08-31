1e8870aab40328d1dc650d3f303ea3e1b0a57137
ahmad <mk4249796@gmail.com>
2026-08-31 15:33:43 +0300

    made the prperty listing desgin (static implementation)

---
+++ djangoairbbn/app/components/properties/PropertyListItem.tsx
@@ -0,0 +1,24 @@
+import Image from "next/image";
+const PropertyListItem = () => {
+    return (
+        <div>
+            <div className=" cursor-pointer relative w-55 mt-5 overflow-hidden aspect-square rounded-xl">
+                <Image
+                fill
+                src="/beach1.avif" alt="beach house"
+                sizes="(max-width: 768px) 768px, (max-width: 1200px) 768px, 768px"
+                className="hover:scale-110 object-cover transition h-full w-full " />
+                
+            </div>
+
+            <div className="mt-2">
+                <p className="text-lg font-bold">Property Name</p>
+            </div>
+            <div className="mt-2">
+                <p className="text-sm text-gray-700 ">200$ <strong>per night</strong></p>
+            </div>
+        </div>
+    )
+}
+
+export default PropertyListItem;
