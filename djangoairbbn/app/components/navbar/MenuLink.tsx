ae57b865757e10c5089010ec05b87ef0267029a2
ahmad <mk4249796@gmail.com>
2026-09-02 15:18:10 +0300

    static user menu

---
+++ djangoairbbn/app/components/navbar/MenuLink.tsx
@@ -0,0 +1,20 @@
+'use client';
+interface MenuLinkProps{
+    label:string;
+    onClick: ()=> void;
+}
+const MenuLink: React.FC<MenuLinkProps> = ({ 
+    label,
+    onClick 
+}) => {
+    return(
+        <div 
+        onClick={onClick}
+        className="px-5 py-4 cursor-pointer hover:bg-gray-100 transiiton">
+            {label}
+        </div>
+    )
+
+}
+
+export default MenuLink;
--- djangoairbbn/app/components/navbar/UserNav.tsx
+++ djangoairbbn/app/components/navbar/UserNav.tsx
@@ -1,4 +1,10 @@
+'use client';
+
+import {useState} from "react";
+import MenuLink from "./MenuLink";
+
 const UserNav=()=>{
+    const [isOpen, setIsOpen]=useState(true);
     return(
         <div className="p-2 relative inline-block border border-gray-500 rounded-full">
             <button className="cursor-pointer flex items-center">
@@ -10,6 +16,19 @@ const UserNav=()=>{
                     <path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2M12 11a4 4 0 1 0 0-8 4 4 0 0 0 0 8z" />
                 </svg>
             </button>
+
+            {isOpen &&(
+                <div className="w-55 absolute top-15 right-0 bg-white border shadow-md rounded-xl flex flex-col cursor-pointer">
+                    <MenuLink
+                    label='log in'
+                    onClick={() => console.log('log in clicked')}
+                    />
+                    <MenuLink
+                    label='sign in'
+                    onClick={() => console.log('sign in clicked')}
+                    />
+                </div>
+            ) }
         </div>
     )
 }
