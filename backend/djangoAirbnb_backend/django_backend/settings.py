9e54cd484a9a787b4e33850227e6fd975e10be49
ahmad <mk4249796@gmail.com>
2026-09-08 17:09:33 +0300

    configured auth

--- backend/djangoAirbnb_backend/django_backend/settings.py
+++ backend/djangoAirbnb_backend/django_backend/settings.py
@@ -1,6 +1,7 @@
 import os
 import dj_database_url
 from dotenv import load_dotenv
+from datetime import timedelta
 load_dotenv()
 """
 Django settings for django_backend project.
@@ -33,6 +34,46 @@ ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
 
 AUTH_USER_MODEL= 'useraccount.User'
 
+SITE_ID= 1
+
+WEBSITE_URL='http://localhost:8000/'
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
+    'ROTATE_REFRESH_TOKENS': False,
+    'BLACKLIST_AFTER_ROTATION': False,
+    'UPDATE_LAST_LOGIN': True,
+    'SIGNING_KEY': 'acomplexkey',
+    'ALGORITHM': 'HS512',
+}
+
+ACCOUNT_USER_MODEL_USERNAME_FIELD=None
+ACCOUNT_SIGNUP_FIELDS = ['email*', 'password1*', 'password2*']
+ACCOUNT_LOGIN_METHODS={'email'}
+ACCOUNT_EMAIL_VERIFICATION='none'
+
+REST_FRAMCEWORK={
+    'DEFAULT_AUTHENTICATION_CLASSES':(
+    'rest_framework_simplejwt.authentication.JWTauthentication',
+    ),
+
+    'DEFAULT_PERMISSION_CLASSES':(
+        'rest_framework_simplejwt.permissions.IsAuthenticated'
+    )
+}
+
+CORS_ALLOWED_ORIGINS=[
+    'http://127.0.0.1:8000',
+    'http://127.0.0.1:3000',
+]
+
+REST_AUTH={
+    'USE_JWT':True,
+    'JWT_AUTH_HTTPONLY':False
+}
+
+
 # Application definition
 
 INSTALLED_APPS = [
@@ -44,11 +85,21 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'useraccount',
     'property',
+    'rest_framework',
+    'rest_framework.authtoken',
+    'rest_framework_simplejwt',
+    'allauth',
+    'allauth.account',
+    'dj_rest_auth',
+    'dj_rest_auth.registration',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'allauth.account.middleware.AccountMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
